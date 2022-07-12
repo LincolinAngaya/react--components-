161bbdfab8caeb0c02de664f6efb3979f43b62e0
LincolinAngaya <lincolinangaya5626@gmail.com>
2022-07-12 14:13:05 +0300

    practice on creating component and giving it props

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,18 @@ import React from "react";
 import SideMenu from "./SideMenu";
 import TopMenu from "./TopMenu";
 import MainContent from "./MainContent";
+import ResultItem from "./ResultItem";
+
 
 function App() {
     return <div className = "app-container" >
-     <SideMenu/>
-     <TopMenu />
-     <MainContent />
-       </div>;
+        <SideMenu / >
+         <TopMenu / >
+        <MainContent / >
+        <ResultItem name="Bubby's" price="$$$" location="Tribeca" />
+      <ResultItem name="Nathan's Famous" price="$" location="Coney Island" />
+
+        </div>;
 }
 
 export default App;
