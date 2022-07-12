d5e3310dadc433b68c63f5f79ddddf7b98c571ac
LincolinAngaya <lincolinangaya5626@gmail.com>
2022-07-12 13:49:46 +0300

    done

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import SideMenu from "./SideMenu";
+import TopMenu from "./TopMenu";
+import MainContent from "./MainContent";
+
+function App() {
+    return <div className = "app-container" >
+     <SideMenu/>
+     <TopMenu />
+     <MainContent />
+       </div>;
+}
+
+export default App;
